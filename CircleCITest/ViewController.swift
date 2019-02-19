6eccc53e2657256b4e83ac5ac9ebcf28eb8a8ae7
Ryan Romanchuk <rromanchuk@gmail.com>
2019-02-18 16:25:32 -0800

    demo app

---
+++ CircleCITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircleCITest
+//
+//  Created by Ryan Romanchuk on 2/18/19.
+//  Copyright © 2019 Ryan Romanchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+#ruby '2.6.1'
+
+gem 'cocoapods'
+gem 'fastlane'
+gem 'xcode-install'
+
+plugins_path = File.join(File.dirname(__FILE__), 'fastlane', 'Pluginfile')
+eval_gemfile(plugins_path) if File.exist?(plugins_path)
+
